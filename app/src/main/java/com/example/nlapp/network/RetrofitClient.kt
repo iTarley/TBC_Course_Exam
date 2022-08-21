1ac687e21867cf0b4721075e4b6b226396cddfe9
Luka <nikuradzeluka0@gmail.com>
2022-08-21 21:31:06 +0400

    currency exchange api implementation

--- app/src/main/java/com/example/nlapp/network/RetrofitClient.kt
+++ app/src/main/java/com/example/nlapp/network/RetrofitClient.kt
@@ -8,6 +8,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 object RetrofitClient {
 
     private const val CRYPTO_URL = "https://api.coingecko.com/"
+    private const val EXCHANGE_URL = "https://api.tbcbank.ge/"
     private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
     val retrofitCrypto: Retrofit by lazy {
@@ -20,5 +21,20 @@ object RetrofitClient {
     val getUsersInfo: ApiInterface by lazy {
         retrofitCrypto.create(ApiInterface::class.java)
     }
+
+
+    private val retrofitExchange:Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(EXCHANGE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+    }
+
+    val getCurrencyInfo: ApiInterface by lazy {
+        retrofitExchange.create(ApiInterface::class.java)
+    }
+
+
 
 }
--- app/src/main/java/com/example/nlapp/ui/exchange/ExchangeFragment.kt
+++ app/src/main/java/com/example/nlapp/ui/exchange/ExchangeFragment.kt
@@ -1,34 +1,83 @@
 package com.example.nlapp.ui.exchange
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.nlapp.MainActivity
+import android.widget.ArrayAdapter
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.nlapp.R
+import com.example.nlapp.databinding.ExchangeFragmentBinding
+import com.example.nlapp.utils.ResponseHandler
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class ExchangeFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ExchangeFragment()
-    }
+    private val viewModel:ExchangeViewModel by viewModels()
+    private var _binding: ExchangeFragmentBinding? = null
+    private val binding get() = _binding!!
 
-    private lateinit var viewModel: ExchangeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.exchange_fragment, container, false)
+        _binding = ExchangeFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        val activity = requireActivity() as? MainActivity
-//        activity?.showNavBar()
+
+        val currencyList = listOf("USD", "GEL", "EUR", "GBP")
+        val adapter = ArrayAdapter(requireContext(), R.layout.list_popup_window_item, currencyList)
+        binding.fromCurrency.setAdapter(adapter)
+        binding.toCurrency.setAdapter(adapter)
+
+        binding.exchangeBtn.setOnClickListener {
+            currExchange()
+        }
+
+    }
+
+
+    private fun currExchange(){
+        val amount = binding.amountEditText.text.toString().toInt()
+        val from = binding.fromCurrency.text.toString()
+        val to = binding.toCurrency.text.toString()
+
+        viewModel.getCurrency(amount,from,to)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.exchangeFlow.collect{
+                    when (it){
+                        is ResponseHandler.Failure -> Log.d("error", "${it.errorMessage}")
+                        is ResponseHandler.Loading -> binding.progressBar.visibility = View.VISIBLE
+                        is ResponseHandler.Success -> {
+                            when (to){
+                                "GEL" -> binding.resultInputText.setText(it.data?.value.toString()+"₾")
+                                "USD" -> binding.resultInputText.setText(it.data?.value.toString()+"$")
+                                "EUR" -> binding.resultInputText.setText(it.data?.value.toString()+"€")
+                                "GBP" -> binding.resultInputText.setText(it.data?.value.toString()+"£")
+                            }
+                            binding.progressBar.visibility = View.INVISIBLE
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
