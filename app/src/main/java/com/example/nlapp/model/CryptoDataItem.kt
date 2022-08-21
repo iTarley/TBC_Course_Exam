c6da5376203eba73bbad7b6da057d4796a36a7b2
Luka <nikuradzeluka0@gmail.com>
2022-08-21 17:24:54 +0400

    commit

--- app/src/main/java/com/example/nlapp/model/CryptoDataItem.kt
+++ app/src/main/java/com/example/nlapp/model/CryptoDataItem.kt
@@ -23,7 +23,7 @@ data class CryptoDataItem(
     @Json(name = "current_price")
     val currentPrice: Double,
     @Json(name = "fully_diluted_valuation")
-    val fullyDilutedValuation: Int?,
+    val fullyDilutedValuation: Long?,
     @Json(name = "high_24h")
     val high24h: Double,
     @Json(name = "id")
@@ -35,7 +35,7 @@ data class CryptoDataItem(
     @Json(name = "low_24h")
     val low24h: Double,
     @Json(name = "market_cap")
-    val marketCap: Int,
+    val marketCap: Long,
     @Json(name = "market_cap_change_24h")
     val marketCapChange24h: Double,
     @Json(name = "market_cap_change_percentage_24h")
@@ -55,5 +55,5 @@ data class CryptoDataItem(
     @Json(name = "total_supply")
     val totalSupply: Double?,
     @Json(name = "total_volume")
-    val totalVolume: Int
+    val totalVolume: Long
 )
--- app/src/main/java/com/example/nlapp/network/RetrofitClient.kt
+++ app/src/main/java/com/example/nlapp/network/RetrofitClient.kt
@@ -1,16 +1,19 @@
 package com.example.nlapp.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitClient {
 
     private const val CRYPTO_URL = "https://api.coingecko.com/"
+    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
     val retrofitCrypto: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(CRYPTO_URL)
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
     }
 
